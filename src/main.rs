956efaea3e46d8b786eff693f3a7828d0193dde3
stolen <0vhbf0@gmail.com>
2023-03-18 16:47:41 -0300

    Player implementation

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,83 @@
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH : i32 = 80;
+const SCREEN_HEIGHT : i32 = 50;
+const FRAME_DURATION : f32 = 75.0;
+
+const TERMINAL_VELOCITY: f32 = 2.0;
+const GRAVITY: f32 = 0.6;
+
 enum GameMode {
     Menu,
     Play,
     Over,
 }
 
+struct Player {
+    x: i32,
+    y: i32,
+    vel: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player { x, y, vel: 0.0 }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(5, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.vel < TERMINAL_VELOCITY {
+            self.vel += GRAVITY;
+        }
+        self.y += self.vel as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.vel = -TERMINAL_VELOCITY;
+    }
+}
+
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        self.mode = GameMode::Over;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press \"SPACE\" to fly");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::Over;
+        }
     }
     fn menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print_centered(10, "Welcome to Flappy Dragon");
+        ctx.print_centered(10, "Welcome to Flappy");
         ctx.print_centered(15, "(P) Play Game");
         ctx.print_centered(17, "(Q) Quit Game");
 
@@ -49,6 +104,8 @@ impl State {
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Play;
     }
 }
